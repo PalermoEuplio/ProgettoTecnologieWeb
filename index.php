62da1d00a6610dd0e07c727d8dff83bd8c31b9cd
vincenzo <42910242+d-Qw4w9WgXc-Q@users.noreply.github.com>
2026-01-13 20:49:34 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+        <head>
+                <meta encoding="utf-8"/>
+                <title>Sito web</title>
+                <link rel="stylesheet" type="text/css" href="style.css"/>
+        </head>
+        <body>
+                <header>
+                        <div id="logo">
+                                <h1>Nome sito</h1>
+                        </div>
+                        <div id="searchbar">
+                                <div id="searchicon">
+                                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg">
+                                        <image width="30" height="30" href="images/searchicon.svg"/>
+                                </svg>
+                                </div>
+                                <input type="text" autocomplete="off" id="searchvalue" placeholder="Cerca..."/>
+                        </div>
+                </header>
+                <aside></aside>
+                <section id="window"></section>
+        </body>
+</html>
