2499d4cf0dd065a53cc6ad9a6dff9db871674b78
vincenzo <42910242+d-Qw4w9WgXc-Q@users.noreply.github.com>
2026-01-14 17:38:04 +0100

    Separata interfaccia dalla homepage e messa in interface.php

--- index.php
+++ index.php
@@ -3,25 +3,11 @@
                 <meta encoding="utf-8"/>
                 <title>Sito web</title>
                 <link rel="stylesheet" type="text/css" href="style.css"/>
+                <link rel="stylesheet" type="text/css" href="interface.css"/>
         </head>
         <body>
-                <header class="panel">
-                        <div id="logo">
-                                <h1>Nome sito</h1>
-                        </div>
-                        <div id="searchbar">
-                                <button id="searchicon">
-                                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg">
-                                        <image width="30" height="30" href="images/searchicon.svg"/>
-                                </svg>
-                                </button>
-                                <input type="text" autocomplete="off" id="searchvalue" placeholder="Cerca..."/>
-                        </div>
-                        <form action="login.php">
-                                <button id="login">Login</button>
-                        </form>
-                </header>
-                <aside class="panel"></aside>
-                <section id="window"></section>
+                <?php include("interface.php")?>
+                <section>
+                </section>
         </body>
 </html>
---
+++ interface.php
@@ -0,0 +1,17 @@
+<header class="panel">
+        <div id="logo">
+                <h1>Nome sito</h1>
+        </div>
+        <div id="searchbar">
+                <button id="searchicon">
+                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg">
+                        <image width="30" height="30" href="images/searchicon.svg"/>
+                </svg>
+                </button>
+                <input type="text" autocomplete="off" id="searchvalue" placeholder="Cerca..."/>
+        </div>
+        <form action="login.php">
+                <button id="login">Login</button>
+        </form>
+</header>
+<aside class="panel"></aside>
