9412bb420e349d3b0ff792f131550d3fa94ba4f7
vincenzo <42910242+d-Qw4w9WgXc-Q@users.noreply.github.com>
2026-01-14 16:27:28 +0100

    Aggiunto tasto login

--- index.php
+++ index.php
@@ -5,20 +5,23 @@
                 <link rel="stylesheet" type="text/css" href="style.css"/>
         </head>
         <body>
-                <header>
+                <header class="panel">
                         <div id="logo">
                                 <h1>Nome sito</h1>
                         </div>
                         <div id="searchbar">
-                                <div id="searchicon">
+                                <button id="searchicon">
                                 <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg">
                                         <image width="30" height="30" href="images/searchicon.svg"/>
                                 </svg>
-                                </div>
+                                </button>
                                 <input type="text" autocomplete="off" id="searchvalue" placeholder="Cerca..."/>
                         </div>
+                        <form action="login.php">
+                                <button id="login">Login</button>
+                        </form>
                 </header>
-                <aside></aside>
+                <aside class="panel"></aside>
                 <section id="window"></section>
         </body>
 </html>
