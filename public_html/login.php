8e428e1b3c4fcccaaa1ee2074a9978813a31d1d0
vincenzo <42910242+d-Qw4w9WgXc-Q@users.noreply.github.com>
2026-01-15 15:07:28 +0100

    Ristrutturato progetto e aggiunta pagina di login di base

---
+++ public_html/login.php
@@ -0,0 +1,23 @@
+<html>
+        <head>
+                <meta encoding="utf-8"/>
+                <title>Login</title>
+                <link rel="stylesheet" type="text/css" href="style.css"/>
+                <link rel="stylesheet" type="text/css" href="login.css"/>
+        </head>
+        <body>
+                <div>
+                        <div id="login" class="panel">
+                                <form id="loginform">
+                                        <h1>Accedi</h1>
+                                        <p>Username: </p>
+                                        <input type="text"></input>
+                                        <p>Password: </p>
+                                        <input type="text"></input>
+
+                                        <button>Accedi</button>
+                                </form>
+                        </div>
+                </div>
+        </body>
+</html>
