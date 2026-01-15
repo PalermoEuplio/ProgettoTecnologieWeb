eb16b721d69c8c882e82547d97f0904f41953149
PalermoEuplio <euppal123456789@gmail.com>
2026-01-15 18:50:49 +0100

    Aggiunto layer utenti alla pagina principale

--- public_html/interface.php
+++ public_html/interface.php
@@ -4,9 +4,9 @@
         </div>
         <div id="searchbar">
                 <button id="searchicon">
-                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg">
-                        <image width="30" height="30" href="images/searchicon.svg"/>
-                </svg>
+                        <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg">
+                                <image width="30" height="30" href="images/searchicon.svg"/>
+                        </svg>
                 </button>
                 <input type="text" autocomplete="off" id="searchvalue" placeholder="Cerca..."/>
         </div>
@@ -14,4 +14,31 @@
                 <button id="login">Login</button>
         </form>
 </header>
-<aside class="panel"></aside>
+<body>
+        <div id="user">
+                <button id="userIcon">
+                        <svg width="70" height="70" viewBox="0 0 32 32">
+                                <image href="images/anonymusUserIcon.svg" width="32" height="32"/>
+                        </svg>
+                </button>
+
+
+                <input id="fastmsg" type="text" autocomplete="off" id="searchvalue" placeholder="A Cosa stai Pensando?..."/>
+                
+                <button id="sendMessegeIcon">
+                        <svg width="43" height="35" viewBox="0 0 32 32">
+                                <image width="32" height="32" href="images/sendMessageIcon.svg"/>
+                        </svg>
+                </button>
+                <button id="addPhoto">
+                        <svg width="43" height="35" viewBox="0 0 32 32">
+                                <image width="32" height="32" href="images/addPictureIcon.svg"/>
+                        </svg>
+                </button>
+        </div>
+
+
+
+        <aside class="panel"></aside>        
+</body>
+
