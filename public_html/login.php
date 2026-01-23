2294b8be6682191aa7dc9c8e82949a48e2533877
GaetanoSir <g.sirignano11@studenti.unisa.it>
2026-01-23 19:17:35 +0100

    scusa

--- public_html/login.php
+++ public_html/login.php
@@ -8,13 +8,17 @@
         <body>
                 <div>
                         <div id="login" class="panel">
+                                <div id="welcome">
+                                        <h1>Bentornato!</h1>
+                                        <p>Non vediamo l'ora di tornare a vedere i tuoi memini...(¬‿¬)</p>
+                                        <img src="images/ranalogin.jpg" height="200" width="200" id="immagine"/>
+                                </div>         
                                 <form id="loginform">
                                         <h1>Accedi</h1>
                                         <p>Username: </p>
                                         <input type="text"></input>
                                         <p>Password: </p>
                                         <input type="text"></input>
-
                                         <button>Accedi</button>
                                 </form>
                         </div>
