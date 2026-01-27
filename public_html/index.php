6327858dec9081f23e7393233ba0dfab140d3e31
GaetanoSir <g.sirignano11@studenti.unisa.it>
2026-01-27 11:55:16 +0100

    Cambio stile pagina di login e rimozione dal file style.css

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,7 @@
         <head>
                 <meta encoding="utf-8"/>
                 <title>HomePage</title>
-                <link rel="stylesheet" type="text/css" href="style.css"/>
+                <link rel="stylesheet" type="text/css"/>
                 <link rel="stylesheet" type="text/css" href="interfacenonloggato.css"/>
         </head>
         <body>
